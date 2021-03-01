7e27dff1f7eae961d0db64a2d316196fcda06f3b
rijnswand <thisroomthatikeep@gmail.com>
2021-03-01 17:56:08 +0100

    initial changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,30 @@ import $ from "jquery";
 import "popper.js";
 import "bootstrap";
 
+// import Swiper bundle with all modules installed
+import Swiper from 'swiper/bundle';
+
+// import Swiper styles
+import 'swiper/swiper-bundle.css';
+
+var menu = ['Slide 1', 'Slide 2', 'Slide 3']
+var mySwiper = new Swiper ('.swiper-container', {
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+			clickable: true,
+        renderBullet: function (index, className) {
+          return '<span class="' + className + '">' + (menu[index]) + '</span>';
+        },
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
